1c8feadd07c5db2cba6f68ad642aa6faaf658cc4
Ilya <ilya-katkov-95@mail.ru>
2024-03-16 21:42:05 +0300

    Docker

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drf_project_2',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.getenv('BASE_PASSWORD')
+        'PASSWORD': '1793',
+        'HOST': 'db',
     }
 }
 
